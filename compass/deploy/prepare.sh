6ad52e22ed630dbd60a48fb26ad020a55d1aed41
baigk <baiguoku@huawei.com>
2015-07-07 17:29:00 +0800

    change venv dir, for shebang length limit

--- compass/deploy/prepare.sh
+++ compass/deploy/prepare.sh
@@ -16,6 +16,7 @@ function prepare_env() {
     mkdir -p $WORK_DIR/vm
     mkdir -p $WORK_DIR/network
     mkdir -p $WORK_DIR/iso
+    mkdir -p $WORK_DIR/venv
 
     if [[ ! -f centos.iso ]];then
         wget -O $WORK_DIR/iso/centos.iso http://192.168.123.11:9999/xh/work/build/work/compass.iso
@@ -30,5 +31,5 @@ function prepare_env() {
     rm -rf $WORK_DIR/mnt
 
     chmod 755 $WORK_DIR -R
-    virtualenv $WORK_DIR/installer/compass-core/venv
+    virtualenv $WORK_DIR/venv
 }
