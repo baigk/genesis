c5896c47dacf3afff2e99b980a9782e93e5d3a51
Chigang(Justin) <chigang@huawei.com>
2015-08-12 04:09:35 -0400

    mod make_repo.sh for generate install packages

--- compass/ci/make_repo.sh
+++ compass/ci/make_repo.sh
@@ -4,16 +4,18 @@ set -ex
 WORK_PATH=$(cd "$(dirname "$0")"; pwd)
 REPO_PATH=${WORK_PATH}/repo
 UBUNTU_TAG="trusty"
+CENTOS_TAG="centos7"
 OPENSTACK_TAG="juno"
 DOCKER_TAG="${UBUNTU_TAG}/openstack-${OPENSTACK_TAG}"
 DOCKER_FILE=${WORK_PATH}/${UBUNTU_TAG}/${OPENSTACK_TAG}/Dockerfile
+DEPLOY_SCRIPT_PATH="/home/chigang/code/compass4nfv/deploy/adapters/ansible/"
 
 if [[ $UID != 0 ]]; then
     echo "You are not root user!"
     exit 1
 fi
 
-if [ ! -d ${REPO_PATH} ]; then
+if [[ ! -d ${REPO_PATH} ]]; then
     mkdir -p ${REPO_PATH}
 fi
 
@@ -24,7 +26,7 @@ else
     echo "docker is already installed!"
 fi
 
-if [ -e ${WORK_PATH}/cp_repo.sh ]; then
+if [[ -e ${WORK_PATH}/cp_repo.sh ]]; then
     rm -f ${WORK_PATH}/cp_repo.sh
 fi
 
@@ -33,6 +35,16 @@ cat <<EOF >${WORK_PATH}/cp_repo.sh
 set -ex
 cp /*ppa.tar.gz /result
 EOF
+
+if [[ -e ${WORK_PATH}/install_packeages.sh ]]; then
+    rm -f ${WORK_PATH}/install_packages.sh
+fi
+
+#generate install_packages.sh
+apt-get install python-yaml -y
+apt-get install python-cheetah -y
+
+python gen_ins_pkg_script.py ${DEPLOY_SCRIPT_PATH} Debian Debian.tmpl
 
 #docker build
 docker build -t ${DOCKER_TAG} -f ${DOCKER_FILE} .
@@ -44,7 +56,15 @@ docker run -t -v ${REPO_PATH}:/result ${DOCKER_TAG}
 IMAGE_ID=$(docker images|grep ${DOCKER_TAG}|awk '{print $3}')
 docker rmi -f ${IMAGE_ID}
 
-if [ -e ${WORK_PATH}/cp_repo.sh ]; then
+#DOCKER_TAG="${CENTOS_TAG}/openstack-${OPENSTACK_TAG}"
+#DOCKER_FILE=${WORK_PATH}/${CENTOS_TAG}/${OPENSTACK_TAG}/Dockerfile
+
+#docker build -t ${DOCKER_TAG} -f ${DOCKER_FILE} .
+
+#mkdir -p ${REPO_PATH}
+#docker run -t -v ${REPO_PATH}:/result ${DOCKER_TAG}
+
+if [[ -e ${WORK_PATH}/cp_repo.sh ]]; then
     rm -f ${WORK_PATH}/cp_repo.sh
 fi
 
